73ca2de4ca16f60303e28bdc943d0b2c34849bee
Simant <developersimant@gmail.com>
2023-05-29 21:32:35 +0545

    adding background

---
+++ src/components/backgroundimg.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import background from images
+const backgroundimg = () => {
+  return (
+    <container>
+        <img src={background} alt="background" />
+    </container>
+  )
+}
+
+export default backgroundimg
--- src/user/signup.jsx
+++ src/user/signup.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Header from '../components/Header';
 
 const signup = () => {
   return (
-    <div>signup</div>
+    <container>
+      <backgroundimg/>
+      <Header/>
+      <div className="body flex column a-center j-center">
+        <div className="text flex column">
+          <h1>Unlimited Movie, Series, cartoons & more..</h1>
+          <h4>Watch Anywhere</h4>
+          <h6>Excited to watch? Enter your email to create or restart</h6>
+        </div>
+        <div className="">
+          <input type ="email"placeholder='Email Address' name="email"/>
+          <input type="password" placeholder='password' name='password'/>
+          <button>Get Started</button>
+        </div>
+        <button>Log In</button>
+      </div>
+
+    </container>
   )
 }
 
